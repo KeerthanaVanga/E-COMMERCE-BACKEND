6471bcc7d4f8a5a84375b874035a3509def429e0
vanga_keerthana <vangakeerthana2018@gmail.com>
2026-02-11 13:12:20 +0530

    changed cookies same site

--- utils/setCookies.js
+++ utils/setCookies.js
@@ -2,7 +2,7 @@ export const setUserAccessTokenCookie = (res, userAccessToken) => {
   res.cookie("userAccessToken", userAccessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: "strict", // or 'None' if cross-site
+    sameSite: "none", // or 'None' if cross-site
     maxAge: 15 * 60 * 1000,
   });
 };
@@ -11,7 +11,7 @@ export const setUserRefreshTokenCookie = (res, userRefreshToken) => {
   res.cookie("userRefreshToken", userRefreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: "strict", // or 'None' if cross-site
+    sameSite: "none", // or 'None' if cross-site
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
@@ -20,7 +20,7 @@ export const setAdminAccessTokenCookie = (res, adminAccessToken) => {
   res.cookie("adminAccessToken", adminAccessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: "Strict", // or 'None' if cross-site
+    sameSite: "none", // or 'None' if cross-site
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
 };
@@ -29,7 +29,7 @@ export const setAdminRefreshTokenCookie = (res, adminRefreshToken) => {
   res.cookie("adminRefreshToken", adminRefreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: "Strict", // or 'None' if cross-site
+    sameSite: "none", // or 'None' if cross-site
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
